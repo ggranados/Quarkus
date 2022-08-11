6c7b88bf98d6027dc66b68808368e67d814ae0ce
Guillermo Granados <guillermo.granados@outlook.com>
2022-08-10 23:16:36 -0400

    [geeting-stated] added rest unit test

--- getting-started/src/test/java/com/example/ExampleResourceTest.java
+++ getting-started/src/test/java/com/example/ExampleResourceTest.java
@@ -3,19 +3,32 @@ package com.example;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class ExampleResourceTest {
+class ExampleResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    void testHelloEndpoint() {
         given()
                 .when().get("/hello")
                 .then()
                 .statusCode(200)
-                .body(is("Hello from RESTEasy Reactive"));
+                .body(is("Hello"));
+    }
+
+    @Test
+    void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given()
+                .pathParam("name", uuid)
+                .when().get("/hello/greeting/{name}")
+                .then()
+                .statusCode(200)
+                .body(is("hello " + uuid));
     }
 
 }
