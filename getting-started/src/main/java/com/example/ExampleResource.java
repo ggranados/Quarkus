1bebc443d307466e33f3e6154440993145d89ad2
Guillermo Granados <guillermo.granados@outlook.com>
2022-08-10 22:59:51 -0400

    [geeting-stated] added service with dependency injection

--- getting-started/src/main/java/com/example/ExampleResource.java
+++ getting-started/src/main/java/com/example/ExampleResource.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +9,19 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class ExampleResource {
 
+    @Inject
+    GreetingService service;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String greeting(String name){
+        return service.greeting(name);
+    }
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello";
     }
 }
